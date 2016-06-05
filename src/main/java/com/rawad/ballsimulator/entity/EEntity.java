96874e690febf6bb212d11ab1423ec219f79f8c2
Rawad Aboudlal <Rawadaboudlal@yahoo.com>
2016-06-05 12:23:44 -0400

    Removed unnecessary code in EEntity and made CollisionSystem look nicer.

--- src/main/java/com/rawad/ballsimulator/entity/EEntity.java
+++ src/main/java/com/rawad/ballsimulator/entity/EEntity.java
@@ -1,30 +1,6 @@
 package com.rawad.ballsimulator.entity;
 
 public enum EEntity {
-	/*/
-	CAMERA(TransformComponent.class, AttachmentComponent.class, UserViewComponent.class),// Transform for scaling, collision for keeping in bounds
-	USER_CONTROLLABLE_CAMERA(TransformComponent.class, MovementComponent.class, UserControlComponent.class, UserViewComponent.class),
-	STATIC(TransformComponent.class, CollisionComponent.class, SelectionComponent.class, RenderingComponent.class),
-	PLAYER(TransformComponent.class, CollisionComponent.class, HealthComponent.class, MovementComponent.class, RollingComponent.class, RenderingComponent.class),
-	USER_CONTROLLABLE_PLAYER(PLAYER.getComponents(), GuiComponent.class, UserControlComponent.class, RandomPositionComponent.class),
-	NETWORKING_PLAYER(PLAYER.getComponents(), GuiComponent.class),// NetworkControlComponent? (or that + UserControlComponent)
-	PLACEABLE(TransformComponent.class, SelectionComponent.class, PlaceableComponent.class, RenderingComponent.class);
-	
-	private final Class<? extends Component>[] components;
-	
-	@SafeVarargs
-	private EEntity(Class<? extends Component>... components) {
-		this.components = components;
-	}
-	
-	@SafeVarargs
-	private EEntity(Class<? extends Component>[] components, Class<? extends Component>... moreComponents) {
-		this(Util.append(components, moreComponents));
-	}
-	
-	public Class<? extends Component>[] getComponents() {
-		return components;
-	}/**/
 	
 	CAMERA("Camera"),
 	PLAYER("Player"),
